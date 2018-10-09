33af4de34ce0fb7f0383e93ef632caf6b8902b88
reviiii <43205544+reviiii@users.noreply.github.com>
2018-10-09 19:24:54 +0200

    Update prices.js

--- prices.js
+++ prices.js
@@ -1,3 +1,3 @@
-var prices = {spruce: 15, jungle: 15, purpur: 20, end_rod: 100}
-var pricePoint = {spruce: 64, jungle:64, purpur: 32, end_rod: 16}
-var storage = {spruce: 13824, jungle: 1728, purpur: 1152, end_rod: 512}
+var prices = {spruce: 15, dark_oak: 15, jungle: 15, oak: 25, purpur: 20, end_rod: 100}
+var pricePoint = {spruce: 64, dark_oak: 64, jungle: 64, oak: 64, purpur: 32, end_rod: 16}
+var storage = {spruce: 13824, dark_oak: 576, jungle: 3456, oak: 576, purpur: 2304, end_rod: 512}
